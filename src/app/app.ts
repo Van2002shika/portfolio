2c3030af63525214e7638d1d33eedbce068a4625
Van2002shika <vanshikaparihaar0@gmail.com>
2025-10-24 22:27:10 +0530

    portfolio

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,8 +5,37 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected title = 'portfolio';
+  fullText = "Hello, I'm Vanshika Parihar";
+  ft = 'Software development Engineer I';
+  displayedText = '';
+  displayedText2 = '';
+  private index = 0;
+  private index2 = 0;
+
+  ngOnInit() {
+    this.typeWriter();
+  }
+
+  typeWriter() {
+    if (this.index < this.fullText.length) {
+      this.displayedText += this.fullText.charAt(this.index);
+      this.index++;
+      setTimeout(() => this.typeWriter(), 100); // typing speed
+    } else {
+      // Wait a bit, then start typing the second line
+      setTimeout(() => this.typeWriterSecond(), 800);
+    }
+  }
+
+  typeWriterSecond() {
+    if (this.index2 < this.ft.length) {
+      this.displayedText2 += this.ft.charAt(this.index2);
+      this.index2++;
+      setTimeout(() => this.typeWriterSecond(), 100);
+    }
+  }
 }
